3c99ea141da636e95a15e8f13b9fe1de732674a0
Suvin Koshy George <70506817+skfreego@users.noreply.github.com>
2022-08-19 23:46:54 +0530

    Add files via upload

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/mnt/c/Users/NN/Documents/work/develper/user-backend/myenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
